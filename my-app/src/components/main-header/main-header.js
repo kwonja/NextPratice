0c46687e525e7f11a9a572e2a67e3f0eb3cd7349
kwonja <kownja87@gmail.com>
2024-10-16 09:54:56 +0900

    2024.10.16 기타컴포넌트

--- my-app/src/components/main-header/main-header.js
+++ my-app/src/components/main-header/main-header.js
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from './main-header.module.css'
+import MainHeaderBackGround from "./mian-header-background";
 export default function MainHeader(){
-    return <header className={classes.header}>
+    return(
+    <>
+    <MainHeaderBackGround/>
+    <header className={classes.header}>
         <Link className ={classes.logo} href="/">
-        <img src={logoImg.src} alt="A plate with food on it"/>
+
+        {/* next에서 제공하는 Image 태그를 사용하면 자동으로 지연로딩을 지원한다 -> 개발자도구를 통해 확인할 수 있음 */}
+        <Image src={logoImg} alt="A plate with food on it" priority/>
             NextLevel Food
         </Link>
 
@@ -18,5 +25,7 @@ export default function MainHeader(){
                 </li>
             </ul>
         </nav>
-    </header>
+    </header>    
+    </>
+)
 }
